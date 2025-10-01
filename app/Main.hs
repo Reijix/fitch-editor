f54f1fbbabf0ba4ee368028988f7888d7949d19c
Leon Vatthauer <leon.vatthauer@fau.de>
2025-10-01 16:11:09 +0200

    work on implementing table in svg, added dragndrop

--- app/Main.hs
+++ app/Main.hs
@@ -1,180 +1,158 @@
 -----------------------------------------------------------------------------
-{-# LANGUAGE CPP               #-}
-{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE OverloadedStrings #-}
+
 -----------------------------------------------------------------------------
 module Main where
------------------------------------------------------------------------------
-import           Miso
-import           Miso.Html.Element as H
-import           Miso.Html.Event as E
-import           Miso.Html.Property as P
-import           Miso.Lens
-import           Miso.String
+
+import Control.Monad (when)
+import qualified Data.Map as M
+import Miso
+  ( App,
+    MisoString,
+    Attribute,
+    Component (events, subs),
+    Effect,
+    PointerEvent (client),
+    ROOT,
+    View,
+    component,
+    consoleLog,
+    io_,
+    mouseSub,
+    ms,
+    onWithOptions,
+    pointerDecoder,
+    preventDefault,
+    run,
+    startApp,
+    text,
+  )
 import qualified Miso.CSS as CSS
-import           Miso.CSS (StyleSheet)
+import qualified Miso.Html.Element as H
+import Miso.Html.Event as E
+import qualified Miso.Html.Property as HP
+import Miso.Lens (Lens, lens, this, use, (.=), (^.))
+import Miso.Svg (text_, tspan_)
+import qualified Miso.Svg.Element as S
+import qualified Miso.Svg.Property as SP
+
 -----------------------------------------------------------------------------
-newtype Model = Model { _value :: Int }
-  deriving (Show, Eq)
------------------------------------------------------------------------------
-instance ToMisoString Model where
-  toMisoString (Model v) = toMisoString v
------------------------------------------------------------------------------
-value :: Lens Model Int
-value = lens _value $ \m v -> m { _value = v }
------------------------------------------------------------------------------
-data Action
-  = AddOne PointerEvent
-  | SubtractOne PointerEvent
-  | SayHelloWorld
-  deriving (Show, Eq)
------------------------------------------------------------------------------
-#ifdef WASM
+#if WASM
 foreign export javascript "hs_start" main :: IO ()
 #endif
 -----------------------------------------------------------------------------
 main :: IO ()
-main = run $ startApp app
+main =
+  run $ startApp app
+
 -----------------------------------------------------------------------------
+
+-- | Component definition (uses 'component' smart constructor)
 app :: App Model Action
-app = (component (Model 0) updateModel viewModel)
-  { events = pointerEvents
-  , styles = [ Sheet sheet ]
-  }
+app =
+  (component emptyModel updateModel viewModel)
+    { subs = [mouseSub HandlePointer],
+      events = M.fromList [("pointerdown", False), ("pointerup", False)]
+    }
+
 -----------------------------------------------------------------------------
-updateModel :: Action -> Transition Model Action
-updateModel = \case
-  AddOne event -> do
-    value += 1
-    io_ $ consoleLog (ms (show event))
-  SubtractOne event -> do
-    value -= 1
-    io_ $ consoleLog (ms (show event))
-  SayHelloWorld ->
-    io_ (consoleLog "Hello World!")
+emptyModel :: Model
+emptyModel = Model {_x = 0.0, _y = 0.0, _active = False}
+
 -----------------------------------------------------------------------------
+updateModel :: Action -> Effect ROOT Model Action -- TODO is ROOT correct??
+-- updateModel (HandlePointer pointer) = this .= client pointer
+updateModel (HandlePointer pointer) =
+  use active >>= \a ->
+    when
+      a
+      ( do
+          let (x, y) = client pointer
+          curr_x .= x
+          curr_y .= y
+          io_ $ do consoleLog "pointer"
+      )
+updateModel (PointerDown _) = active .= True
+updateModel (PointerUp _) = active .= False
+
+-----------------------------------------------------------------------------
+data Action
+  = PointerDown PointerEvent
+  | PointerUp PointerEvent
+  | HandlePointer PointerEvent
+  deriving (Show, Eq)
+
+-----------------------------------------------------------------------------
+data Model = Model {_x :: Double, _y :: Double, _active :: Bool} deriving (Eq)
+
+active :: Lens Model Bool
+active = lens _active $ \model n -> model {_active = n}
+
+curr_x :: Lens Model Double
+curr_x = lens _x $ \model n -> model {_x = n}
+
+curr_y :: Lens Model Double
+curr_y = lens _y $ \model n -> model {_y = n}
+
+data Formula = Formula -- TODO
+
+data Rule = Rule -- TODO
+
+data Line = Line Formula Rule
+
+data Proof = ProofLine Line | SubProof [Formula] [Proof] Line
+
+-- disable text-highlighting during drag and drop. `preventDefault`
+onPD :: (PointerEvent -> Action) -> Attribute Action
+onPD f =
+  onWithOptions
+    preventDefault
+    "pointerdown"
+    pointerDecoder
+    (\action _ -> f action)
+
+-----------------------------------------------------------------------------
+viewLine :: MisoString -> Line -> View Model Action
+viewLine dy (Line _ _) = text_ [ SP.x_ "50", SP.y_ "50", SP.dy_ dy ] [tspan_ [ ] ["FORMULA"], tspan_ [ SP.dx_ "30px" ] ["RULE"]]
+
 viewModel :: Model -> View Model Action
-viewModel x = H.div_
-  [ P.class_ "counter-container" ]
-  [ H.h1_
-    [ P.class_ "counter-title"
+viewModel (Model x y _) =
+  H.div_
+    [E.onPointerUp PointerUp]
+    [ -- H.h1_ [] ["Fitch Editor"],
+      H.svg_
+        [ CSS.style_
+            [ CSS.borderStyle "solid",
+              CSS.height "700px",
+              CSS.width "100%"
+            ]
+        ]
+        [ text_
+            [ SP.x_ $ ms (x - 50),
+              SP.y_ $ ms (y - 52)
+            ]
+            [ text $ ms $ show (round x :: Integer, round y :: Integer)
+            ],
+          S.g_
+            [onPD PointerDown]
+            [ S.rect_
+                [ SP.x_ $ ms (x - 50),
+                  SP.y_ $ ms (y - 50),
+                  CSS.style_
+                    [ CSS.fill "yellow",
+                      CSS.stroke "purple",
+                      CSS.strokeWidth "2"
+                    ],
+                  SP.width_ "100px",
+                  SP.height_ "100px"
+                ]
+            ],
+          S.g_
+            [onPD PointerDown]
+            [ viewLine "0em" (Line Formula Rule), viewLine "1em" (Line Formula Rule), viewLine "2em" (Line Formula Rule)
+            ]
+        ]
     ]
-    [ "🍜 Miso counter"
-    ]
-  , H.div_
-    [ P.class_ "counter-display"
-    ]
-    [ text (ms x)
-    ]
-  , H.div_
-    [ P.class_ "buttons-container"
-    ]
-    [ H.button_
-      [ E.onPointerDown AddOne
-      , P.class_ "decrement-btn"
-      ] [text "+"]
-    , H.button_
-      [ E.onPointerDown SubtractOne
-      , P.class_ "increment-btn"
-      ] [text "-"]
-    ]
-  ]
------------------------------------------------------------------------------
-sheet :: StyleSheet
-sheet =
-  CSS.sheet_
-  [ CSS.selector_ ":root"
-    [ "--primary-color" =: "#4a6bff"
-    , "--primary-hover" =: "#3451d1"
-    , "--secondary-color" =: "#ff4a6b"
-    , "--secondary-hover" =: "#d13451"
-    , "--background" =: "#f7f9fc"
-    , "--text-color" =: "#333"
-    , "--shadow" =: "0 4px 10px rgba(0, 0, 0, 0.1);"
-    , "--transition" =: "all 0.3s ease;"
-    ]
-  , CSS.selector_ "body"
-    [ CSS.fontFamily "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif"
-    , CSS.display "flex"
-    , CSS.justifyContent "center"
-    , CSS.alignItems "center"
-    , CSS.height "100vh"
-    , CSS.margin "0"
-    , CSS.backgroundColor (CSS.var "background")
-    , CSS.color (CSS.var "text-color")
-    ]
-  , CSS.selector_ ".counter-container"
-    [ CSS.backgroundColor CSS.white
-    , CSS.padding (CSS.rem 2)
-    , CSS.borderRadius (CSS.px 12)
-    , CSS.boxShadow "shadow"
-    , CSS.textAlign "center"
-    ]
-  , CSS.selector_ ".counter-display"
-    [ CSS.fontSize "5rem"
-    , CSS.fontWeight "bold"
-    , CSS.margin "1CSS.rem 0"
-    , CSS.transition "var(--transition)"
-    ]
-  , CSS.selector_ ".buttons-container"
-    [ CSS.display "flex"
-    , CSS.gap "1rem"
-    , CSS.justifyContent "center"
-    , CSS.marginTop "1.5rem"
-    ]
-  , CSS.selector_ "button"
-    [ CSS.fontSize "1.5rem"
-    , CSS.width "3rem"
-    , CSS.height "3rem"
-    , CSS.border "none"
-    , CSS.borderRadius "50%"
-    , CSS.cursor "pointer"
-    , CSS.transition "var(--transition)"
-    , CSS.color CSS.white
-    , CSS.display "flex"
-    , CSS.alignItems "center"
-    , CSS.justifyContent "center"
-    ]
-  , CSS.selector_ ".increment-btn"
-    [ CSS.backgroundColor (CSS.var "primary-color")
-    ]
-  , CSS.selector_ ".increment-btn:hover"
-    [ CSS.backgroundColor (CSS.var "primary-hover")
-    , CSS.transform "translateY(-2px)"
-    ]
-  , CSS.selector_ ".decrement-btn"
-    [ CSS.backgroundColor (CSS.var "secondary-color")
-    ]
-  , CSS.selector_ ".decrement-btn:hover"
-    [ CSS.backgroundColor (CSS.var "secondary-hover")
-    , CSS.transform "translateY(-2px)"
-    ]
-  , CSS.keyframes_ "pulse"
-    [ CSS.pct 0 =:
-      [ CSS.transform "scale(1)"
-      ]
-    , CSS.pct 50 =:
-      [ CSS.transform "scale(1.1)"
-      ]
-    , CSS.pct 100 =:
-      [ CSS.transform "scale(1)"
-      ]
-    ]
-  , CSS.selector_ ".counter-display.animate"
-    [ CSS.animation "pulse 0.3s ease"
-    ]
-  , CSS.media_ "(max-width: 480px)"
-    [ ".counter-container" =:
-      [ CSS.padding (CSS.rem 1.5)
-      ]
-    , ".counter-display" =:
-      [ CSS.fontSize (CSS.rem 3)
-      ]
-    , "button" =:
-      [ CSS.fontSize (CSS.rem 1.2)
-      , CSS.width (CSS.rem 2.5)
-      , CSS.width (CSS.rem 2.5)
-      ]
-    ]
-  ]
+
 -----------------------------------------------------------------------------
