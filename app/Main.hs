dfa2a205a1ff1dd631f9ab5868b11daef18466de
Leon Vatthauer <leon.vatthauer@fau.de>
2025-10-08 10:33:04 +0200

    sync

--- app/Main.hs
+++ app/Main.hs
@@ -31,12 +31,12 @@ import Miso
 import qualified Miso.CSS as CSS
 import qualified Miso.Html.Element as H
 import Miso.Html.Event as E
-import qualified Miso.Html.Property as HP
 import Miso.Lens (Lens, lens, this, use, (.=), (^.))
 import Miso.Svg (text_, tspan_)
 import qualified Miso.Svg.Element as S
 import qualified Miso.Svg.Property as SP
 import qualified Data.List as L
+import Data.Either
 
 -----------------------------------------------------------------------------
 #if WASM
@@ -72,14 +72,16 @@ updateModel (HandlePointer pointer) =
           let (x, y) = client pointer
           cursor_x Miso.Lens..= x
           cursor_y Miso.Lens..= y
-          io_ $ do consoleLog "pointer"
+          -- io_ $ do consoleLog "pointer"
       )
-updateModel (PointerDown _) = active Miso.Lens..= True
+updateModel (PointerDown n _) = do
+  io_ $ consoleLog $ ms n
+  active Miso.Lens..= True
 updateModel (PointerUp _) = active Miso.Lens..= False
 
 -----------------------------------------------------------------------------
 data Action
-  = PointerDown PointerEvent
+  = PointerDown Int PointerEvent
   | PointerUp PointerEvent
   | HandlePointer PointerEvent
   deriving (Show, Eq)
@@ -104,13 +106,29 @@ cursor_y = Miso.Lens.lens _cursor_y $ \model y -> model {_cursor_y = y}
 proof :: Miso.Lens.Lens Model Proof
 proof = Miso.Lens.lens _proof $ \model p -> model {_proof = p}
 
-data Formula = Formula deriving Eq -- TODO
+data Formula = Formula deriving (Show, Eq) -- TODO
 
-data Rule = Rule deriving Eq -- TODO
+data Rule = Rule deriving (Show, Eq) -- TODO
 
 data Line = Line Formula Rule deriving Eq
 
 data Proof = EMPTYP | ProofLine Line | SubProof [Formula] [Proof] Line deriving Eq
+
+pLength :: Proof -> Int
+pLength EMPTYP             = 0
+pLength (ProofLine l)      = 1
+pLength (SubProof fs ps _) = foldr (\p n -> pLength p + n) (L.length fs + 1) ps
+
+pLookup :: Proof -> Int -> Either Line Formula
+pLookup EMPTYP n                                = error "Tried (!!) on EMPTYP"
+pLookup (ProofLine l) 0                         = Left l
+pLookup (ProofLine _) _                         = error "Tried (!!) on ProofLine with n > 0"
+pLookup (SubProof fs _ _) n | n < L.length fs   = Right $ fs L.!! n
+pLookup (SubProof _ [] l) 0                     = Left l
+pLookup (SubProof _ [] _) _                     = error "Tried (!!) on SubProof fs [] l with n > 0"
+pLookup (SubProof fs (p:_) _) n | n < pLength p = pLookup p n
+pLookup (SubProof fs (_:ps) l) n | otherwise    = pLookup (SubProof fs ps l) n
+
 
 -- disable text-highlighting during drag and drop. `preventDefault`
 onPD :: (PointerEvent -> Action) -> Attribute Action
@@ -124,33 +142,33 @@ onPD f =
 -----------------------------------------------------------------------------
 toEm :: Int -> MisoString
 toEm n = ms (show n ++ "em")
-viewLine :: Int -> Line -> View Model Action
-viewLine dy (Line _ _) = text_ [ SP.x_ "50", SP.y_ "50", SP.dy_ $ toEm dy ] [tspan_ [ ] ["FORMULA"], tspan_ [ SP.dx_ "30px" ] ["RULE"]]
-
-viewRule :: Int -> Rule -> View Model Action
-viewRule dy _ = text_ [ SP.x_ "50", SP.y_ "50", SP.dy_ $ toEm dy ] [ "RULE" ]
-
-viewFormula :: Int -> Formula -> View Model Action
-viewFormula dy _ = text_ [ SP.x_ "50", SP.y_ "50", SP.dy_ $ toEm dy ] [ "FORMULA" ]
-
-viewProof :: (Int, Int) -> Proof -> View Model Action
-viewProof (x,y) p = snd $ _viewProof 0 p
-  where 
-    _viewProof :: Int -> Proof -> (Int, View Model Action)
-    _viewProof _ EMPTYP = (0, S.g_ [] [])
-    _viewProof dy (ProofLine l) = (dy + 1, S.g_ [] [ viewLine dy l ])
-    _viewProof dy (SubProof fs ps l) = (dy + length allLines, S.g_ [] allLines)
-      where
-        allLines :: [View Model Action]
-        allLines = do
-          let (dy', fs') = L.mapAccumL (\acc f  -> (acc + 1, viewFormula acc f)) dy fs
-          let (dy'', ps') = L.mapAccumL (\acc p -> _viewProof acc p) dy' ps
-          fs' ++ ps' ++ [viewLine dy'' l]
-
-
 
 exProof :: Proof
 exProof = SubProof [Formula, Formula] [ProofLine (Line Formula Rule)] (Line Formula Rule)
+-----------------------------------------------------------------------------
+-- VIEWS
+viewLine :: Int -> Int -> Line -> View Model Action
+viewLine n dy (Line f r) = text_ [ SP.x_ "50", SP.y_ "50", SP.dy_ $ toEm dy, onPD (PointerDown n) ] [tspan_ [ ] [ text $ ms $ (show f ++ show n) ], tspan_ [ SP.dx_ "30px" ] [ text $ ms $ (show r ++ show n) ]]
+
+viewRule :: Int -> Int -> Rule -> View Model Action
+viewRule n dy r = text_ [ SP.x_ "50", SP.y_ "50", SP.dy_ $ toEm dy, onPD (PointerDown n) ] [ text $ ms $ show r ]
+
+viewFormula :: Int -> Int -> Formula -> View Model Action
+viewFormula n dy f = text_ [ SP.x_ "50", SP.y_ "50", SP.dy_ $ toEm dy, onPD (PointerDown n) ] [ text $ ms $ show f ]
+
+viewProof :: Int -> (Int, Int) -> Proof -> View Model Action
+viewProof n (x,y) p = snd $ _viewProof n 0 p
+  where 
+    _viewProof :: Int -> Int -> Proof -> (Int, View Model Action)
+    _viewProof n _ EMPTYP = (0, S.g_ [] [])
+    _viewProof n dy (ProofLine l) = (dy + 1, viewLine n dy l)
+    _viewProof n dy (SubProof fs ps l) = (dy + length allLines, S.g_ [] allLines)
+      where
+        allLines :: [View Model Action]
+        allLines = do
+          let (acc', fs') = L.mapAccumL (\acc f  -> (acc + 1, viewFormula (n + acc) (dy + acc) f)) 0 fs
+          let (acc'', ps') = L.mapAccumL (\acc p -> _viewProof (n + acc) (dy + acc) p) acc' ps
+          fs' ++ ps' ++ [viewLine (n + acc'') (dy + acc'') l]
 
 viewModel :: Model -> View Model Action
 viewModel (Model x y _ _) =
@@ -171,7 +189,7 @@ viewModel (Model x y _ _) =
             [ text $ ms $ show (round x :: Integer, round y :: Integer)
             ],
           S.g_
-            [onPD PointerDown]
+            [onPD (PointerDown 100)]
             [ S.rect_
                 [ SP.x_ $ ms (x - 50),
                   SP.y_ $ ms (y - 50),
@@ -186,7 +204,7 @@ viewModel (Model x y _ _) =
             ],
             S.g_
               []
-              [viewProof (50, 50) exProof]
+              [viewProof 0 (50, 50) exProof]
           -- S.g_
           --   [onPD PointerDown]
           --   [ viewLine 0 (Line Formula Rule), viewLine "1em" (Line Formula Rule), viewLine "2em" (Line Formula Rule)
