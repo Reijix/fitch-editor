3dfd18c645e649b6fe8e03d07bd8a2e6debc5631
Leon Vatthauer <leon.vatthauer@fau.de>
2025-10-16 15:42:14 +0200

    start html rework, add styling

--- app/App.hs
+++ app/App.hs
@@ -4,7 +4,8 @@ import Control.Monad (when)
 import qualified Data.Map as M
 import Miso
   ( App,
-    Component (events, subs),
+    CSS (Href),
+    Component (events, styles, subs),
     Effect,
     PointerEvent (client),
     ROOT,
@@ -24,8 +25,6 @@ import Miso.Html.Event as E (onPointerUp)
 import qualified Miso.Html.Property as HP
 import Miso.Lens (use, (.=))
 import Miso.Svg (text_)
-import qualified Miso.Svg.Element as S
-import qualified Miso.Svg.Property as SP
 import Syntax
 import Views
 
@@ -39,6 +38,7 @@ runApp emptyModel = run $ startApp app
     app =
       (component emptyModel updateModel viewModel)
         { subs = [mouseSub HandlePointer],
+          styles = [Href "assets/style.css"],
           events = M.fromList [("pointerdown", False), ("pointerup", False), ("mouseover", False)]
         }
 
@@ -65,52 +65,6 @@ viewModel :: forall rule formula. (Show rule) => (Show formula) => Model rule fo
 viewModel (Model x y _ prf) =
   H.div_
     [E.onPointerUp PointerUp]
-    [ H.p_ [] ["Test"], -- H.h1_ [] ["Fitch Editor"],
-    -- TODO viewBox instead of heigth and width!
-      H.svg_
-        [ CSS.style_
-            [ CSS.borderStyle "solid",
-              CSS.height "700px",
-              CSS.width "100%"
-            ]
-        ]
-        -- TODO use foreignObject for lines, and then use onInput
-        -- https://github.com/haskell-miso/miso-todomvc/blob/bc133d50971e18c137fbe3e2d0e05bc1a6b5c231/src/Main.hs#L310
-        [ S.foreignObject_ [SP.height_ "1em", SP.width_ "5em", SP.x_ $ ms (200 :: Int), SP.y_ $ ms (200 :: Int)] [H.input_ [HP.placeholder_ "asd"]],
-          text_
-            [ SP.x_ $ ms (x - 50),
-              SP.y_ $ ms (y - 52)
-            ]
-            [ text $ ms $ show (round x :: Integer, round y :: Integer)
-            ],
-          S.g_
-            []
-            [ S.rect_
-                [ SP.x_ "0",
-                  SP.y_ "0",
-                  CSS.style_ [CSS.fill "red"],
-                  SP.width_ "100px",
-                  SP.height_ "33%"
-                ],
-              S.rect_
-                [ SP.x_ "0",
-                  SP.y_ "33%",
-                  CSS.style_ [CSS.fill "yellow"],
-                  SP.width_ "100px",
-                  SP.height_ "33%"
-                ],
-              S.rect_
-                [ SP.x_ "0",
-                  SP.y_ "66%",
-                  CSS.style_ [CSS.fill "orange"],
-                  SP.width_ "100px",
-                  SP.height_ "33%"
-                ]
-            ],
-          S.g_
-            []
-            [viewProof 0 (100, 50) prf]
-        ]
-    ]
+    [viewProof 0 (100, 50) prf]
 
 -----------------------------------------------------------------------------
--- app/Views.hs
+++ app/Views.hs
@@ -12,6 +12,8 @@ import Miso
     preventDefault,
     text,
   )
+import qualified Miso.Html.Element as H
+import qualified Miso.Html.Property as HP
 import Miso.Property (textProp)
 import Miso.Svg (text_, tspan_)
 import qualified Miso.Svg.Element as S
@@ -21,33 +23,39 @@ import Syntax
 href_ :: MisoString -> Attribute action
 href_ = textProp "href"
 
+-- TODO
 viewDragIcon :: (Int, Int) -> Int -> View (Model rule formula) Action
-viewDragIcon (x, y) dy = S.image_ [href_ "draggable.svg", SP.width_ "1ex", SP.height_ "1ic", SP.x_ $ ms x, SP.y_ $ ms "calc(100px + 5em)"]
+viewDragIcon (x, y) dy = H.img_ [HP.src_ "draggable.svg", HP.height_ "16"]
 
 -- VIEWS
+-- TODO
 viewLine ::
   forall rule formula.
   (Show rule) =>
   (Show formula) =>
   Int -> (Int, Int) -> Int -> Line rule formula -> View (Model rule formula) Action
-viewLine n (x, y) dy (Line f r) = S.g_ [SP.x_ $ ms x, SP.y_ $ ms y] [viewDragIcon (x, y) dy, text_ [SP.x_ $ ms x, SP.y_ $ ms y, SP.dy_ $ toEm dy, onPD (PointerDown n)] [tspan_ [] [text $ ms (show f ++ show n)], tspan_ [SP.dx_ "30px"] [text $ ms (show r ++ show n)]]]
+viewLine n (x, y) dy (Line f r) =
+  H.p_
+    []
+    [viewDragIcon (x, y) dy, text $ ms (show f ++ show n), text $ ms (show r ++ show n)]
 
+-- TODO
 viewRule ::
   forall rule formula.
   (Show rule) =>
   (Show formula) =>
   Int -> (Int, Int) -> Int -> RuleSpec rule formula -> View (Model rule formula) Action
-viewRule n (x, y) dy r = text_ [SP.x_ $ ms x, SP.y_ $ ms y, SP.dy_ $ toEm dy, onPD (PointerDown n)] [text $ ms $ show r]
+viewRule n (x, y) dy r = H.p_ [onPD (PointerDown n)] [text $ ms $ show r]
 
+-- TODO
 viewFormula ::
   forall rule formula.
   (Show rule) =>
   (Show formula) =>
   Int -> (Int, Int) -> Int -> formula -> View (Model rule formula) Action
-viewFormula n (x, y) dy f = text_ [SP.x_ $ ms x, SP.y_ $ ms y, SP.dy_ $ toEm dy, onPD (PointerDown n)] [text $ ms $ show f]
+viewFormula n (x, y) dy f = H.p_ [onPD (PointerDown n)] [viewDragIcon (x, y) dy, text $ ms $ show f]
 
--- TODO intersperse every line with rectangles that have a "onmouseover" such that they turn red when action_ is true and hovered over.
--- TODO wrap each proof in g_ for grabbing it.
+-- TODO
 viewProof ::
   forall rule formula.
   (Show rule) =>
@@ -56,9 +64,11 @@ viewProof ::
 viewProof n (x, y) p = snd $ _viewProof n 0 p
   where
     _viewProof :: Int -> Int -> Proof rule formula -> (Int, View (Model rule formula) Action)
-    _viewProof n dy (PhantomProof m) = (n, S.g_ [] $ L.replicate m $ text_ [SP.x_ $ ms x, SP.y_ $ ms y, SP.dy_ $ toEm dy] [""])
+    -- TODO empty lines
+    _viewProof n dy (PhantomProof m) = (n, H.div_ [] $ L.replicate m $ H.p_ [] [""])
+    -- TODO
     _viewProof n dy (ProofLine l) = (dy + 1, viewLine n (x, y) dy l)
-    _viewProof n dy (SubProof fs ps l) = (dy + length allLines, S.g_ [] allLines)
+    _viewProof n dy (SubProof fs ps l) = (dy + length allLines, H.div_ [] allLines)
       where
         allLines :: [View (Model rule formula) Action]
         allLines = do
